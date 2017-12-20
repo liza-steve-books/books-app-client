4aadeaec58359e3caf4b0633d4fe23ab47512a88
Steven Carpenter <stevegcarpenter@gmail.com>
2017-12-20 08:56:36 -0800

    updated the url for database

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,7 +3,7 @@
 var app = app || {};
 
 (function (module){
-  var __API_URL__ = 'https://ag-sh-booklist.herokuapp.com';
+  var __API_URL__ = 'https://lo-sc-booklist.herokuapp.com';
   // var __API_URL__ = 'http://localhost:3000';
   Book.all = [];
 
