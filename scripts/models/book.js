ebe79cbe05c80cbe695e40a2fac04a06831d0359
Steven Carpenter <stevegcarpenter@gmail.com>
2017-12-19 22:19:37 -0800

    Removed unnecessary code and added the .eslintrc file

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -35,50 +35,5 @@ var app = app || {};
       })
   };
 
-  Book.prototype.insertRecord = function(callback) {
-      $.post('/api/v1/books', {author: this.author, title: this.title, isbn: this.isbn, image_url: this.image_url, description: this.description})
-        .then(console.log)
-        .then(callback);
-    };
-
-  //Maybe later?
-  // Book.truncateTable = callback => {
-  //   $.ajax({
-  //     url: '/api/v1/books',
-  //     method: 'DELETE',
-  //   })
-  //     .then(console.log)
-  //     .then(callback);
-  // };
-
-  // Maybe later?  
-  // Book.prototype.updateRecord = function(callback) {
-  //   $.ajax({
-  //     url: `/api/v1/books/${this.article_id}`,
-  //     method: 'PUT',
-  //     data: {
-  //       author: this.author,
-  //       authorUrl: this.authorUrl,
-  //       body: this.body,
-  //       category: this.category,
-  //       publishedOn: this.publishedOn,
-  //       title: this.title,
-  //       author_id: this.author_id
-  //     }
-  //   })
-  //     .then(console.log)
-  //     .then(callback);
-  // };
-
-  // Maybe later?
-  // Book.prototype.deleteRecord = function(callback) {
-  //   $.ajax({
-  //     url: `/api/v1/books/${this.book_id}`,
-  //     method: 'DELETE'
-  //   })
-  //     .then(console.log)
-  //     .then(callback);
-  // };
-
   module.Book=Book;
 })(app);
