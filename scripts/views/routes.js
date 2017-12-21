6027c862278198606a6722fa5073a4988c1b0e53
Steven Carpenter <stevegcarpenter@gmail.com>
2017-12-21 11:33:56 -0800

    Enabled the /new route and added anchor links into the html buttons.

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -2,7 +2,7 @@
 
 page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/:id', ctx => app.Book.fetchOne(ctx.params.id, app.bookView.initDetailPage));
-// page('/add', () => app.bookView.initAddNewPage());
+page('/new', () => app.bookView.initAddNewPage());
 
 // Catch all to take user back to home page
 // Doesn't work currently
